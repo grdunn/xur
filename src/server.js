d1c56f5da5111ffd6dc1241490c0711e3b7c715b
Greg Dunn <grdunn@gmail.com>
2023-09-14 11:37:52 -0400

    port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = require('./lib/router');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use('/api', router);
